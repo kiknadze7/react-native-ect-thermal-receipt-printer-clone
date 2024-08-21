ae8d3586f8defeed91b77912ac28f733c52bc867
Meme <mister.kiknadze@gmail.com>
2024-08-21 21:01:35 +0400

    chore: Update package name and URLs to reflect repository clone

---
+++ android/build/generated/source/buildConfig/debug/com/kiknadze7/react/printer/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kiknadze7.react.printer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.kiknadze7.react.printer";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ android/src/main/java/com/kiknadze7/react/printer/adapter/PrinterDeviceId.java
@@ -0,0 +1,8 @@
+package com.kiknadze7.react.printer.adapter;
+
+/**
+ * Created by xiesubin on 2017/9/21.
+ */
+
+public class PrinterDeviceId {
+}
